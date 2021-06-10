94e50ae340acbcd65e382755628e45087d4a57ae
manuelsanchez2 <manusansan22@gmail.com>
2021-06-10 18:53:44 +0200

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,117 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.black45,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('assets/manu.png'),
+              ),
+              Text(
+                'Manuel Sánchez',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Web & Mobile Dev'.toUpperCase(),
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 30,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+34 661 667 769',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 30,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'manuel.webtech@gmx.de',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.location_on,
+                      size: 30,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'Hamburg, Germany',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
